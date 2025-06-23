8c87b0cbfb90f8540e1aa1a77ea40afc6af1181b
Sumithasri09 <ssumithaanand@gmail.com>
2025-06-23 16:07:23 +0530

    fix: update to listen on 0.0.0.0 and use process.env.PORT

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('This is sumithasri!'));
+app.get('/', (req, res) => res.send('This is Sumithasri!'));
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App running on http://0.0.0.0:${port}`);
+});
